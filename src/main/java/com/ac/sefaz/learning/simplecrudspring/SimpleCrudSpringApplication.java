f8f80f7a3c0259e6571fa7bba598ed8f5401e2cf
Leonardo Nascimento <Developerheart>
2021-08-09 10:07:49 -0400

    Starter do projeto

---
+++ src/main/java/com/ac/sefaz/learning/simplecrudspring/SimpleCrudSpringApplication.java
@@ -0,0 +1,13 @@
+package com.ac.sefaz.learning.simplecrudspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleCrudSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleCrudSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ac/sefaz/learning/simplecrudspring/SimpleCrudSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ac.sefaz.learning.simplecrudspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleCrudSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
